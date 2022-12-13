3507381c66ff7f26c68a4144ebbf4b83dae42ed6
_crhis_ <crhisnokia@live.com>
2022-12-13 18:33:55 -0300

    feat: seletores dom

---
+++ seletores-dom/script.js
@@ -0,0 +1,14 @@
+console.log("Olá mundo!")
+
+const titulo =  document.getElementById("titulo");
+console.log(titulo)
+
+const textos = document.getElementsByClassName("texto-simples");
+console.log(textos)
+console.log(textos[0])
+
+const segundoTitulo = document.querySelector("div h2");
+console.log(segundoTitulo)
+
+const textosPorClasse =  document.querySelectorAll(".texto-simples");
+console.log(textosPorClasse)
